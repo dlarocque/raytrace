5cf785aaa6add4ac448380a9f2c9fc3a9ef87aa3
dlarocque <larocq17@myumanitoba.ca>
2021-12-26 16:31:19 -0600

    Added antialiasing

--- main.cpp
+++ main.cpp
@@ -1,34 +1,38 @@
 #include <iostream>
 #include "vec3.h"
 #include "ray.h"
+#include "sphere.h"
+#include "hittable_list.h"
+#include "float.h"
+#include "camera.h"
 
-double hit_sphere(const vec3& sphere_center, double radius, const ray& r) {
-    vec3 oc = r.origin() - sphere_center;
-    double a = dot(r.direction(), r.direction());
-    double b = 2.0 * dot(oc, r.direction());
-    double c = dot(oc, oc) - radius*radius;
-    double discriminant = b*b - 4*a*c;
-    
-    if (discriminant < 0) {
-        return -1.0;
+#include <memory>
+
+
+vec3 ray_color(const ray& r, const hittable& world) {
+    hit_record rec;
+    if (world.hit(r, 0.0, MAXFLOAT, rec)) {
+        return 0.5 * vec3(rec.normal.x()+1, rec.normal.y()+1, rec.normal.z()+1);
     } else {
-        return (-b - sqrt(discriminant)) / (2.0 * a);
+        vec3 unit_direction = unit_vector(r.direction());
+        float t = 0.5 * (unit_direction.y() + 1.0);
+        return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
     }
 }
 
-vec3 ray_color(const ray& r) {
-    double t = hit_sphere(vec3(0.0, 0.0, -1.0), 0.5, r);
-    if (t > 0.0) {
-        vec3 N = unit_vector(r.at(t) - vec3(0.0, 0.0, -1.0));
-        return 0.5 * vec3(N.x()+1, N.y()+1, N.z()+1);
-    }
-    vec3 unit_direction = unit_vector(r.direction());
-    return (1.0-t) * vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
+hittable_list random_scene() {
+    hittable_list world;
+
+    world.add(std::make_shared<sphere>(point(0, 0, -1), 0.5));
+    world.add(std::make_shared<sphere>(point(0, -100.5, 0), 100));
+
+    return world;
 }
 
 int main() {
     int nx = 200;
     int ny = 100;
+    int ns = 50;
 
     std::cout << "P3\n" << nx << " " << ny << "\n255\n";
 
@@ -37,12 +41,23 @@ int main() {
     vec3 vertical(0.0, 2.0, 0.0);
     vec3 origin(0.0, 0.0, 0.0);
 
+    auto world = random_scene();
+    camera cam;
+
     for(int j = ny-1; j >= 0; j--) {
         for(int i = 0; i < nx; i++) {
-            float u = float(i) / float(nx);
-            float v = float(j) / float (ny);
-            ray r(origin, lower_left_corner + u*horizontal + v*vertical);
-            vec3 col = ray_color(r);
+            vec3 col(0,0,0);
+            
+            // antialiasing
+            for(int s = 0; s < ns; s++) {
+                float u = float(i + drand48()) / float(nx);
+                float v = float(j + drand48()) / float (ny);
+                ray r = cam.get_ray(u, v);
+                vec3 p = r.at(2.0);
+                col += ray_color(r, world);
+            }
+            col /= float(ns);
+
             int ir = int(255.99*col[0]);
             int ig = int(255.99*col[1]);
             int ib = int(255.99*col[2]);
---
+++ sphere.h
@@ -0,0 +1,48 @@
+#ifndef SPHEREH
+#define SPHEREH
+
+#include "hittable.h"
+
+
+class sphere : public hittable {
+    public:
+        sphere() {}
+        sphere(const point& cen, double r) : center(cen), radius(r) {};
+        virtual bool hit(const ray& r, double t_min, double t_max, hit_record& rec) const;
+    
+    public:
+        point center;
+        double radius;
+};
+
+bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) const {
+    vec3 oc = r.origin() - center;
+    auto a = r.direction().length_squared();
+    auto half_b = dot(oc, r.direction());
+    auto c = oc.length_squared() - radius*radius;
+
+    auto discriminant = half_b * half_b - a*c;
+    if (discriminant < 0) {
+        return false;
+    }
+    
+    auto sqrtd = sqrt(discriminant);
+    
+    // find the nearest root in the range [t_min, t_max]
+    auto root = (-half_b - sqrtd) / a;
+    if (t_min > root || root > t_max) {
+        root = (-half_b + sqrtd) / a;
+        if(root < t_min || root > t_max) {
+            return false;
+        }
+    }
+
+    rec.t = root;
+    rec.p = r.at(rec.t);
+    vec3 outward_normal = (rec.p - center) / radius;
+    rec.set_face_normal(r, outward_normal);
+
+    return true;
+}
+
+#endif
